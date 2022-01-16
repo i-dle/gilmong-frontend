e95147f08f1e31ab2c472feb0e54e3bc85d1efa8
kimuichan <leerabi577@gmail.com>
2022-01-16 21:24:04 +0900

    ⚙️ :: 각종 라이브러리 기본 세팅 및 style 세팅

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import GlobalStyle from "./styles/globalstyle";
+import { RecoilRoot } from "recoil";
 ReactDOM.render(
   <React.StrictMode>
-    <GlobalStyle></GlobalStyle>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <RecoilRoot>
+      <GlobalStyle></GlobalStyle>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </RecoilRoot>
   </React.StrictMode>,
   document.getElementById("root")
 );
