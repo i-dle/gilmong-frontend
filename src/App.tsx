4e34b6297c82294dfd22eb892a6f2045298de89d
kimuichan <leerabi577@gmail.com>
2022-01-16 21:23:18 +0900

    ⚡️ :: route 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Header from "./components/common/Header";
+import LoadMap from "./components/loadMap";
 import Main from "./components/main";
+import Modal from "./components/modal";
+import Process from "./components/process";
+import ShowPost from "./components/showpost";
 function App() {
   return (
     <>
+      <Modal></Modal>
       <Header></Header>
       <Routes>
         <Route path="/" element={<Main></Main>}></Route>
+        <Route path="/roadmap" element={<LoadMap></LoadMap>}></Route>
+        <Route path="/showpost/:id" element={<ShowPost></ShowPost>}></Route>
+        <Route path="/process" element={<Process></Process>}></Route>
       </Routes>
     </>
   );
