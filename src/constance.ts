bd4c420b3da3c8f9a54abb9945353a8455dbbe03
kimuichan <leerabi577@gmail.com>
2022-01-16 21:19:54 +0900

    ⚡️ :: 직업의 유형을 관리하는 constance 추가

---
+++ src/constance.ts
@@ -0,0 +1,103 @@
+export const CategoryLists: CategoryListType[] = [
+  {
+    categoryId: 1,
+    title: "사업관리",
+  },
+  {
+    categoryId: 2,
+    title: "경영·회계·사무",
+  },
+  {
+    categoryId: 3,
+    title: "금융·보험",
+  },
+  {
+    categoryId: 4,
+    title: "교육·자연·사회과학",
+  },
+  {
+    categoryId: 5,
+    title: "법률·경찰·소방·국방",
+  },
+  {
+    categoryId: 6,
+    title: "보건·의료",
+  },
+  {
+    categoryId: 7,
+    title: "사회복지·종교",
+  },
+  {
+    categoryId: 8,
+    title: "문화·예술·디자인·방송",
+  },
+  {
+    categoryId: 9,
+    title: "운전·운송",
+  },
+  {
+    categoryId: 10,
+    title: "영업판매",
+  },
+  {
+    categoryId: 11,
+    title: "경비·청소",
+  },
+  {
+    categoryId: 12,
+    title: "숙박·여행·오락·스포츠",
+  },
+  {
+    categoryId: 13,
+    title: "음식서비스",
+  },
+  {
+    categoryId: 14,
+    title: "건설",
+  },
+  {
+    categoryId: 15,
+    title: "기계",
+  },
+  {
+    categoryId: 16,
+    title: "재료",
+  },
+  {
+    categoryId: 17,
+    title: "화학·바이오",
+  },
+  {
+    categoryId: 18,
+    title: "섬유·의복",
+  },
+  {
+    categoryId: 19,
+    title: "전기·전자",
+  },
+  {
+    categoryId: 20,
+    title: "정보통신",
+  },
+  {
+    categoryId: 21,
+    title: "식품가공",
+  },
+  {
+    categoryId: 22,
+    title: "인쇄·목재·가구·공예",
+  },
+  {
+    categoryId: 23,
+    title: "환경·에너지·안전",
+  },
+  {
+    categoryId: 24,
+    title: "농림어업",
+  },
+];
+
+interface CategoryListType {
+  title: string;
+  categoryId: number;
+}
