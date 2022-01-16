6916da48fb84c162772baa1d74678b8063c5d7b9
kimuichan <leerabi577@gmail.com>
2022-01-16 21:19:29 +0900

    ⚡️ :: category의 상태를 관리하는 atom

---
+++ src/recoil/categoryState.ts
@@ -0,0 +1,31 @@
+import { atom } from "recoil";
+
+export const categoryListState = atom<boolean[]>({
+  key: "categoryListState",
+  default: [
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    true,
+    false,
+    false,
+    false,
+    false,
+  ],
+});
